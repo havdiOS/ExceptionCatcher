92c912f25916f08e2eca661ac595a14c1d2d18c8
Ha.vu1 <Ha.vu1@onemount.com>
2025-03-12 14:50:49 +0700

    ExceptionCatcher

--- ExceptionCatcher/ExceptionCatcher.h
+++ ExceptionCatcher/ExceptionCatcher.h
@@ -7,6 +7,14 @@
 
 #import <Foundation/Foundation.h>
 
+//! Project version number for ExceptionCatcher.
+FOUNDATION_EXPORT double ExceptionCatcherVersionNumber;
+
+//! Project version string for ExceptionCatcher.
+FOUNDATION_EXPORT const unsigned char ExceptionCatcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExceptionCatcher/PublicHeader.h>
+
 NS_INLINE NSException * _Nullable tryBlock(void(^_Nonnull tryBlock)(void)) {
     @try {
         tryBlock();
